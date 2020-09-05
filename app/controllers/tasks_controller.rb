8a512d922136b483999d30e7427cd411d883f73a
mitsuhiro <strawberrycarts@yahoo.co.jp>
2020-09-05 03:43:04 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in , only: [:index, :show]
+  before_action :correct_user, only: [:destroy]
+
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -9,11 +12,11 @@ class TasksController < ApplicationController
 
   def new
     #Task.new(content: 'sample')
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -55,4 +58,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
